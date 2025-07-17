e39f804f5b65cf01e2d462b663275f5b4de87fbb
Jorge <jorgejapao00@gmail.com>
2025-07-17 20:24:16 -0300

    main screen script added

---
+++ scenes/MainScreenScript.gd
@@ -0,0 +1,13 @@
+extends VBoxContainer
+
+
+func _on_new_game_pressed():
+	pass # Replace with function body.
+	
+
+func _on_about_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
