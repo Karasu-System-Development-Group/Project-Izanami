79897f3229c081b44a6d1ad611e4222041bb2584
Jorge <jorgejapao00@gmail.com>
2025-07-18 18:33:33 -0300

    sounds added to settings button

--- scenes/MainScreenScript.gd
+++ scenes/MainScreenScript.gd
@@ -10,13 +10,29 @@ func _on_new_game_pressed():
 	
 func _on_new_game_mouse_entered():
 	hoverSound.play()
+
+
+## SETTINGS ##	
+func _on_settings_pressed():
+	print(2)
+	selectSound.play()
+
+func _on_settings_mouse_entered():
+	hoverSound.play()
+
 	
+
+## ABOUT ##
+
 func _on_about_pressed():
 	print(2)
 	selectSound.play()
 
 func _on_about_mouse_entered():
 	hoverSound.play()
+
+
+## QUIT ##
 
 func _on_quit_pressed():
 	selectSound.play()
