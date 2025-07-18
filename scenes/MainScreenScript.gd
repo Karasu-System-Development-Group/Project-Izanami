a18734fbab9fb81dc238d5ea9e3fdedc0979a3ce
Jorge <jorgejapao00@gmail.com>
2025-07-17 21:12:03 -0300

    audio stream player imported. Function to play sound when hovering buttons added

--- scenes/MainScreenScript.gd
+++ scenes/MainScreenScript.gd
@@ -1,13 +1,22 @@
 extends VBoxContainer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer  # Assuming AudioStreamPlayer is a child of this node
 
 
 func _on_new_game_pressed():
 	pass # Replace with function body.
 	
+func _on_new_game_mouse_entered():
+	audio_stream_player.play()
+	
 
 func _on_about_pressed():
 	pass # Replace with function body.
 
+func _on_about_mouse_entered():
+	audio_stream_player.play()
 
 func _on_quit_pressed():
 	get_tree().quit()
+	
+func _on_quit_mouse_entered():
+	audio_stream_player.play()
