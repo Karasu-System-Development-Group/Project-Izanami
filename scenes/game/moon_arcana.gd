e21b764cfa395b1ee6588f3d61d1d44defa868e3
Jorge <jorgejapao00@gmail.com>
2025-07-21 10:46:55 -0300

    moon arcana script updated to extend from TextureButton

--- scenes/game/moon_arcana.gd
+++ scenes/game/moon_arcana.gd
@@ -1,13 +1,5 @@
-extends NinePatchRect
+extends TextureButton
 
-var moon = preload("res://assets/arcanas/monochrome/18.png")
-var original_texture
-
-func _ready():
-	original_texture = texture
 
 func _on_mouse_entered():
-	texture = moon
-
-func _on_mouse_exited():
-	texture = original_texture
+	pass # Replace with function body.
