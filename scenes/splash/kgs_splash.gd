cb158a889648f439c2f8ca1b4c5fb37345a09eaa
Jorge <jorgejapao00@gmail.com>
2025-07-22 14:51:07 -0300

    texture button logic added

--- scenes/splash/kgs_splash.gd
+++ scenes/splash/kgs_splash.gd
@@ -7,3 +7,5 @@ func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/main_screen.tscn")
 
 
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_screen.tscn")
