536f019c853ca6c1c167804d97f22f07c48f5523
Jorge <jorgejapao00@gmail.com>
2025-10-09 15:12:00 -0300

    camera limit and last room visited variables added to char

--- scripts/char.gd
+++ scripts/char.gd
@@ -3,11 +3,18 @@ extends CharacterBody2D
 @onready var anim = $AnimatedSprite2D
 @onready var footsteps = $footsteps
 @export var speed = 400
+@onready var camera = $Camera2D
 var direction = Vector2.ZERO
 var rng = RandomNumberGenerator.new()
+var lastRoom = "outside"
+
 
 func _ready():
 	anim.play("idle")
+	camera.enabled = false
+	camera.limit_left = 10
+	camera.limit_right = 10
+
 
 func _physics_process(delta):
 	direction = Vector2.ZERO
