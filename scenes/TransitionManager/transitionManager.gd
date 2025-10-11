b205c81883a3c3b33b9b826402b76688b6aa4333
Jorge <jorgejapao00@gmail.com>
2025-10-11 09:35:17 -0300

    audio transition added to transitionManager

--- scenes/TransitionManager/transitionManager.gd
+++ scenes/TransitionManager/transitionManager.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 @onready var transition = $transition
 
-func play_transition():
+func play_transition(totalTime:=1.5):
 	Char.set_process_input(false)
 	Char.set_physics_process(false)
 	Char.set_process(false)
@@ -10,7 +10,7 @@ func play_transition():
 	var tween = create_tween()
 	tween.tween_property(transition, "modulate:a", 1.0, 1.0)
 	await tween.finished
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(totalTime).timeout
 	var tween_out = create_tween()
 	tween_out.tween_property(transition, "modulate:a", 0.0, 1.0)
 	await tween_out.finished
@@ -18,3 +18,18 @@ func play_transition():
 	Char.set_process_input(true)
 	Char.set_physics_process(true)
 	Char.set_process(true)
+
+
+func play_audio_transition(audio_player, play_time: float = 5.0, fade_time: float = 2.0):
+	audio_player.volume_db = -40
+	audio_player.play()
+	var tween := create_tween()
+	tween.tween_property(audio_player, "volume_db", 0, fade_time)
+	await tween.finished
+
+	await get_tree().create_timer(play_time).timeout
+
+	tween = create_tween()
+	tween.tween_property(audio_player, "volume_db", -40, fade_time)
+	await tween.finished
+	audio_player.stop()
