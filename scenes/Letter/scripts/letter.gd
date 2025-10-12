5681e29faacc049241bf706c127f59a9f98d5fe9
Jorge <jorgejapao00@gmail.com>
2025-10-12 08:09:37 -0300

    show message script updated to play audio

--- scenes/Letter/scripts/letter.gd
+++ scenes/Letter/scripts/letter.gd
@@ -4,6 +4,9 @@ extends Node2D
 @onready var letterText = $letterObject/Node2D/letterText
 @onready var letter =$letterObject/Node2D
 
+
+@export var isLetterVisible = true
+
 func fade_in(duration=1.0):
 	letter.modulate.a = 0
 	letter.visible = true
@@ -21,6 +24,7 @@ func writeOnLetter(text):
 	
 func show_message(text):
 	Char.canMove(false)
+	TransitionManager.play_audio_transition($letterObject/Node2D/letterSound, -1, 2.0)
 	await self.writeOnLetter(text)
 	await self.fade_in()
 
