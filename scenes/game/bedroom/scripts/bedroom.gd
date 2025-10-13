c9e4fa0f4cc301684bda5a2255a5829d3012df93
Jorge <jorgejapao00@gmail.com>
2025-10-13 11:30:58 -0300

    bedroom spawnpoint added

--- scenes/game/bedroom/scripts/bedroom.gd
+++ scenes/game/bedroom/scripts/bedroom.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+func _on_tree_entered():
+	Char.visible = true
+	Char.position = $SpawnPoint.position
+	Char.camera.enabled = true
+	Char.camera.limit_right = $house/wall2/CollisionShape2D.global_position.x
+	Char.camera.limit_left = $house/wall/CollisionShape2D.global_position.x
+
+func _ready():
+	if Char.bedRoomBoxIsVisible:
+		$bedroomBox.visible = true
+		$bedRoomFurnitrune.visible = false
+	else:
+		$bedRoomFurnitrune.visible = true
+		$bedroomBox.visible = false
