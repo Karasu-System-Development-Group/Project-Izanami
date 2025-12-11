8f95ca756863b96934271ace63e1c49f76cf334e
Jorge <jorgejapao00@gmail.com>
2025-12-11 10:14:27 -0300

    kgs splash updated

--- scenes/splash/kgs_splash.gd
+++ scenes/splash/kgs_splash.gd
@@ -3,6 +3,10 @@ extends Control
 func _ready():
 	$ksg_logo/Timer.start()
 	$CrowSound.play()
+	await get_tree().create_timer(1).timeout
+	TransitionManager.play_transition()
+
+
 
 func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/main_screen.tscn")
